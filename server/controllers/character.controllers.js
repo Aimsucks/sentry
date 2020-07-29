19cdb832e49ee1f24cf48ff2e9201de4b7383bdb
Aimsucks <contact@aimsucks.space>
2020-07-29 14:40:02 -0400

    Set a new character to a main if the main is deleted

--- server/controllers/character.controllers.js
+++ server/controllers/character.controllers.js
@@ -29,7 +29,21 @@ module.exports.setMain = async (req, res) => {
 module.exports.delete = async (req, res) => {
   try {
     const id = req.params.id
+
+    console.group(`Beginning deletion process for character with ID ${id}`)
+
+    const character = await models.character.findOne({ where: { id: id, userId: req.user.id } })
+    const count = await models.character.count({ where: { userId: req.user.id } })
+    if (character.main && count > 1) {
+      console.log(`Character with ID ${character.id} is a main`)
+      await models.character.update({ main: true }, { where: { userId: req.user.id, main: false }, plain: true, returning: true, limit: 1, order: [['name', 'ASC']] })
+      console.log('Made a new character a main')
+    }
+
     await models.character.destroy({ where: { id: id, userId: req.user.id } })
+    console.groupEnd()
+    console.log(`Deleted character with ID ${req.user.id}`)
+
     res.send({ message: `Deleted character with ID ${id}` })
   } catch (err) {
     console.error(err)
