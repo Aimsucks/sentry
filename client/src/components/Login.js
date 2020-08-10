53f5215202de76bdaee19ae612047137421119b3
Aimsucks <contact@aimsucks.space>
2020-08-10 12:37:19 -0400

    Move function to top of class

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,21 +4,21 @@ import { Row, Col, Button } from 'antd'
 import { LoginOutlined } from '@ant-design/icons'
 
 export default class Login extends Component {
+  handleLoginClick = () => {
+    window.open('http://localhost:5000/auth/discord', '_self')
+  }
+
   render () {
     return (
       <>
         <Row className='centered-text'>
           <Col>
             <Button onClick={this.handleLoginClick} type='primary' icon={<LoginOutlined />} size='large'>
-                  Login
+                Login
             </Button>
           </Col>
         </Row>
       </>
     )
   }
-
-  handleLoginClick = () => {
-    window.open('http://localhost:5000/auth/discord', '_self')
-  };
 }
